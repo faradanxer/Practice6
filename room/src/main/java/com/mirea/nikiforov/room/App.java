38a90f815c7133d74f9834e0865b49b630885b35
Ivan <nim060222@gmail.com>
2021-06-01 22:02:24 +0300

    Initial commit

---
+++ room/src/main/java/com/mirea/nikiforov/room/App.java
@@ -0,0 +1,26 @@
+package com.mirea.nikiforov.room;
+
+import android.app.Application;
+import androidx.room.*;
+
+public class App extends Application {
+    public static App instance;
+    private AppDatabase database;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        database = Room.databaseBuilder(this, AppDatabase.class, "database")
+                .allowMainThreadQueries()
+                .build();
+    }
+
+    public static App getInstance() {
+        return instance;
+    }
+
+    public AppDatabase getDatabase() {
+        return database;
+    }
+}
